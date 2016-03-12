d5c866c6fe9a2b199469bf09c92205b8d0a90298
srgg6701 <srgg67@gmail.com>
2016-03-12 12:52:49 +0300

    добавление массированного консольного отладчика

--- js/drag-and-drop-columns.js
+++ js/drag-and-drop-columns.js
@@ -1,3 +1,12 @@
+var debugCnt;
+function showArgs(){
+    var args=[];
+    for(var i= 0, j=arguments.length; i<j; i++){
+        args.push(arguments[i]);
+    }
+    return args;
+}
+/**/
 window.onload = function () {
     // солнце взошло, растоманы! Пора распространять урожай ☺
     window.dragStore = setupDragStore(); //console.log('setupDragStore', window.dragStore);
@@ -5,9 +14,11 @@ window.onload = function () {
     // группы карточек по статусам
     window.dragStore.setListeners('.column', 'column');
     window.dragStore.setListeners('.task', 'issue');
+    console.log('%c======================================', 'color: rebeccapurple');
 };
 // тут будут закрома
 function setupDragStore() {
+    console.groupCollapsed('setupDragStore', showArgs(arguments));
     var draggedElement,
         draggedElementsPanel={},
         eventsMap = { /**
@@ -24,12 +35,18 @@ function setupDragStore() {
             drop:       { column: dropColumn, issue: dropIssue },
             dragend:    dragEnd
         }; // приватная переменная
-    return {
+    var setupData= {
         getDragElement: function (key) {
-            return key? draggedElementsPanel[key] : draggedElement;
+            console.group('dragStore.getDragElement', showArgs(arguments));
+            //return key? draggedElementsPanel[key] : draggedElement;
+            var element = key? draggedElementsPanel[key] : draggedElement;
+                console.log('element: ', element);
+            console.groupEnd();
+            return element;
         },
         // вызывается в dragStart
         setDragElement: function (element, key) {
+            console.group('dragStore.setDragElement', showArgs(arguments));
             key ? /** сохранить перетащенный на нижнюю панель элемент
                     чтобы исключить дублирование (далее будет сверяться)
                     по id задачи */
@@ -39,43 +56,57 @@ function setupDragStore() {
                     нужно для проверки -- исключить дублирование элементов
                     в колонках */
                 : draggedElement = element;
+                //--------------------debug
+                var el=(key)?  draggedElementsPanel[key]:draggedElement;
+                console.log('set element to: ', el);
+                //--------------------debug end
+            console.groupEnd();
         },
         // удалить из набора перетащенных на нижнюю панель задач текущую
         removeDraggedElementPanel: function(key){
+            console.groupCollapsed('dragStore.removeDraggedElementPanel', showArgs(arguments));
             delete draggedElementsPanel[key];
+            console.groupEnd();
         },
         setListeners: function (selector, elementsKey, events) {
+            console.groupCollapsed('dragStore.setListeners', showArgs(arguments));
             var /**
                 контейнер пар [событие: функциия], то, что будет передаваться
                 в качестве параметров при установке наблюдателей */
                 eventsSet,
                 // элементы, к которым будем присоединять наблюдателей:
                 elements = document.querySelectorAll(selector);
+                console.log('elements: ', elements);
             /**
              * Если набор событий не передан, извлекаем их (и функции) все */
             if (!events) {
                 eventsSet = eventsMap;
             } else { // если передаем, переформируем набор присоединяемых функций
                 eventsSet = {};
+                console.groupCollapsed('form eventsSet');
                 eventsMap.forEach(function (key) {
                     eventsSet[key] = eventsMap[key];
+                    console.log('eventsSet['+key+']', eventsSet[key]);
                 });
-            }
-            /*console.log('setListeners', {
-                selector:selector,
-                eventsSet:eventsSet, elements:elements
-            });*/
+                console.groupEnd();
+            }   console.log('eventsSet', eventsSet);
+                console.groupCollapsed('%cset listeners', 'color: violet');
             for (var i = 0, j = elements.length, method; i < j; i++) {
                 for (var event in eventsSet) {
                     if (eventsSet.hasOwnProperty(event)) {
                         method = (typeof eventsSet[event] == 'object')?
                             eventsSet[event][elementsKey] : eventsSet[event];
+                        console.log({ element: elements[i], event: event, method: method });
                         elements[i].addEventListener(event, method, false);
                     }
                 }
             }
+                console.groupEnd();
+            console.groupEnd();
         }
-    }
+    };  console.log('return %csetupData ', 'color:green',setupData);
+    console.groupEnd();
+    return setupData;
 }
 
 /**
@@ -83,85 +114,176 @@ function setupDragStore() {
  * @param method
  */
 function handleClassMoving(method) {
+    console.groupCollapsed('%chandleClassMoving', 'font-weight:normal; color:orange', showArgs(arguments));
+        console.log({ method: method, classList: this.classList });
     this.classList[method]('moving');
+        console.log('classList after removing: ', this.classList );
+    console.groupEnd();
 }
 /**
  * Добавить/удалить класс для объекта, НАД которым перемещаем
  * @param method
  */
 function handleClassOver(method) {
+    console.groupCollapsed('handleClassOver', 'font-weight:normal; color:goldenrod', showArgs(arguments));
+        console.log({ method: method, classList: this.classList });
     this.classList[method]('over');
+        console.log('classList after removing: ', this.classList );
+    console.groupEnd();
 }
-// универсальная(?)
+/**
+ * Предотвращает "всплывание" события
+ * Применяет к активному элементу вызвов функции назначения класса "moving"
+ * Сохраняет перемещаемый элемент в приватной пер. draggedElement объекта dragStore
+ * Назначает "эффект перетаскивания"
+ * Определяет данные, которые будут перемещены из старого расположения в новое
+ * @param e ─ event
+ */
 function dragStart(e) {
+    debugCnt=1;
+    console.groupCollapsed('%cdragStart', 'background-color:rgb(180,180,255); padding:4px 10px',{
+        srcElement:arguments[0].srcElement,
+        target:arguments[0].target,
+        toElement:arguments[0].toElement
+    }, showArgs(arguments));
     if (e.stopPropagation) { // предотвратить дальнейшее распространение
         e.stopPropagation();
     }
-    /*var draggedPrevClass = dragStore.getDragElement();
-    if(draggedPrevClass){
-        if(draggedPrevClass.classList.length && draggedPrevClass.classList.contains('task')){
-            if(this.classList.contains('column')) {
-                console.log('.column after .task, break', {
-                    draggedPrevClass:draggedPrevClass,
-                    this:this.classList
-                });
-                return false;
-            }
-        }
-    }*/
     handleClassMoving.call(this, 'add');
     // сохранить текущий активный элемент для обработки при следующих событиях
     dragStore.setDragElement(this);
-
-    console.log('this, saved', this, dragStore.getDragElement());
-
     e.dataTransfer.effectAllowed = 'move';
     e.dataTransfer.setData('text/html', this.innerHTML);
+        console.log('e.dataTransfer',  e.dataTransfer);
+    console.groupEnd();
 }
-// универсальная(?)
+/**
+ * Применяется к ПАССИВНОМУ элементу, в область над которым
+ * входит перетаскиваемый элемент;
+ * В настоящий момент ─ только назначает класс "over"
+ * @param e ─ event
+ */
+function dragEnter(e) {
+    if(debugCnt==2) console.groupEnd(); // свернуть предыдущую пачку dragOver
+    console.groupCollapsed('%cdragEnter', 'font-weight:normal; color: blue',{
+        srcElement:arguments[0].srcElement,
+        target:arguments[0].target,
+        toElement:arguments[0].toElement
+    }, showArgs(arguments));
+    if (e.stopPropagation) { // предотвратить дальнейшее распространение
+        e.stopPropagation();
+    }
+    handleClassOver.call(this, 'add');
+        console.log('this: ', this);
+    console.groupEnd();
+    if(debugCnt==1) console.groupCollapsed('dragOver %cgrouped', 'font-weight:normal; background-color:yellow');
+}
+/**
+ * Предотвращает вызов события по умолчанию;
+ * Назначает "эффект перетаскивания" для элемента
+ * @param e ─ event
+ * @returns {boolean}
+ */
 function dragOver(e) {
+    console.groupCollapsed('%cdragOver', 'font-weight:normal; color:darkorange',{
+        srcElement:arguments[0].srcElement,
+        target:arguments[0].target,
+        toElement:arguments[0].toElement
+    }, showArgs(arguments));
     e.preventDefault();
     e.dataTransfer.dropEffect = 'move';
+        console.log({ e:e, 'e.dataTransfer':e.dataTransfer});
+    console.groupEnd();
+    if(debugCnt==1) debugCnt=2;
+    if(debugCnt==3) debugCnt=4;
     return false;
 }
-// универсальная(?)
-function dragEnter(e) {
-    // добавить класс к блоку с посадочной площадкой
-    handleClassOver.call(this, 'add');
-}
-// универсальная(?)
-function dragLeave(e) { //console.log('dragLeave, this:', this);
-    // удалить класс с перемещаемого блока
+/**
+ * Применяет к пассивному элементу функцию удаления класса "over"
+ * @param e ─ event
+ */
+function dragLeave(e) {
+    console.groupCollapsed('%cdragLeave', 'font-weight:normal; color: rgb(255,0,255)',{
+        srcElement:arguments[0].srcElement,
+        target:arguments[0].target,
+        toElement:arguments[0].toElement
+    }, showArgs(arguments));
+    if (e.stopPropagation) { // предотвратить дальнейшее распространение
+        e.stopPropagation();
+    }
     handleClassOver.call(this, 'remove');
+        console.log('this:', this);
+    console.groupEnd();
+    if(debugCnt==2) {
+        console.groupCollapsed('dragOver %cgrouped', 'font-weight:normal; background-color:yellow');
+        debugCnt=3;
+    }
+    if(debugCnt==4) {
+        console.groupEnd();
+        debugCnt=5;
+        console.groupCollapsed('dragOver %cgrouped', 'font-weight:normal; background-color:yellow');
+    }
 }
-function dragEnd(e) { //console.log('dragEnd, this:', this);
+/**
+ * Применяет к активному элементу функцию удаления класса "moving"
+ * @param  e ─ event
+ */
+function dragEnd(e) {
+    console.groupCollapsed('%cdragEnd', 'background-color: #333; color: white; padding:4px 10px',{
+        srcElement:arguments[0].srcElement,
+        target:arguments[0].target,
+        toElement:arguments[0].toElement
+    }, showArgs(arguments));
+    if (e.stopPropagation) { // предотвратить дальнейшее распространение
+        e.stopPropagation();
+    }
     handleClassMoving.call(this, 'remove');
+        console.log('this:', this);
     var targetAreaOver, targetAreaMoving;
-    if (targetAreaMoving = document.querySelector('.moving'))
+    // clarify: разобраться, почуму без этого карточки не перемещаются в нижний блок!
+    if (targetAreaMoving = document.querySelector('.moving')){
         handleClassMoving.call(targetAreaMoving, 'remove');
+    }
     // задержка для красоты ☻
     setTimeout(function () {
         if (targetAreaOver = document.querySelector('.over'))
             handleClassOver.call(targetAreaOver, 'remove');
     }, 200);
+    console.groupEnd();
+    console.log('%c*******************************************************', 'color: orange');
+    debugCnt=null;
 }
-function initDropping(e) {
+/**
+ * Выполнить стандартные действия перед "сбрасыванием" элемента
+ * @param e
+ * @returns {*}
+ */
+function prepareToDrop(e) {
+    console.groupCollapsed('handleClassOver', showArgs(arguments));
     if (e.stopPropagation) { // предотвратить дальнейшее распространение
         e.stopPropagation();
-    }
+    }   console.log('return: ', dragStore.getDragElement());
+    console.groupEnd();
     // получить последний сохранённый элемент
     return dragStore.getDragElement();
 }
+/**
+ * Если перемещаем карточку, а не группу
+ * @param e
+ * @returns {boolean}
+ */
 function dropColumn(e) {
+    console.groupEnd();
+    // clarify: Нельзя ли унифицировать?
+    console.groupCollapsed('%сdropColumn', 'color:orange', showArgs(arguments));
     /*if (e.stopPropagation) { // предотвратить дальнейшее распространение
      e.stopPropagation();
      }*/
-    var draggedElement = initDropping(e);//dragStore.getDragElement();
-
-    console.log('dropColumn', {
-        draggedElement: draggedElement, this: this,
+    var draggedElement = prepareToDrop(e);//dragStore.getDragElement();
+    console.log({
+        draggedElement: draggedElement, this: this/*,
         draggedElementParentId: draggedElement.parentNode.id,
-        thisParentId: this.parentNode.id
+        thisParentId: this.parentNode.id*/
     });
     // Если собираемся сбрасывать не туда же, откуда пришли
     if (draggedElement != this) {
@@ -170,40 +292,77 @@ function dropColumn(e) {
          if(draggedElement.parentNode.id!=this.parentNode.id)
          return false;
          }*/
-        // Set the source column's HTML to the HTML of the columnwe dropped on.
+        // Set the source element HTML to the HTML of the element dropped on.
         draggedElement.innerHTML = this.innerHTML;
+        console.log('set draggedElement.innerHTML as this.innerHTML: ', draggedElement.innerHTML);
         this.innerHTML = e.dataTransfer.getData('text/html');
-    }
+        console.log('apply this.innerHTML from e.dataTransfer: ', this.innerHTML);
+    }   console.log('%creturns false', 'color: navy');
+    console.groupEnd();
     return false;
 }
-function dropIssue(e) {
-    var draggedElement = initDropping(e);
-    console.log('dropIssue', {e: e, draggedElement:draggedElement});
+/**
+ * Сбросить карточку
+ * @param e
+ */
+function dropIssue(e) { // clarify: Нельзя ли унифицировать?
+    console.groupEnd();
+    console.groupCollapsed('%cdropIssue', 'color:orange', showArgs(arguments));
+    var draggedElement = prepareToDrop(e);
+    console.log('draggedElement', draggedElement);
+    console.groupEnd();
 }
+/**
+ * Получить id задачи
+ * @param element
+ * @returns {string}
+ */
 function getTaskId(element){
+    console.groupCollapsed('%cgetTaskId', 'color:blue', showArgs(arguments));
+    console.log('return: ', element.id.substr(4));
+    console.groupEnd();
     return element.id.substr(4);
 }
 // копировать task на нижнюю панель
 function dropOnRow(e) {
-    var draggedElement = initDropping(e),
+    console.groupEnd();
+    console.groupCollapsed('%cdropOnRow', 'font-weight:normal;color:rebeccapurple',{
+        srcElement:arguments[0].srcElement,
+        target:arguments[0].target,
+        toElement:arguments[0].toElement
+    }, showArgs(arguments));
+    var draggedElement = prepareToDrop(e),
         taskId = getTaskId(draggedElement),
         draggedElementPanel = dragStore.getDragElement(taskId),
         row = e.target, clone;
+    console.log({ draggedElement:draggedElement, taskId:taskId, draggedElementPanel:draggedElementPanel, row:row });
     if(!draggedElementPanel || draggedElement.innerHTML != draggedElementPanel.innerHTML){
         clone = draggedElement.cloneNode(true);
+        console.log('clone: ', clone);
         //
         row.appendChild(clone);
+        console.log('row after adding clone: ', row);
         dragStore.setDragElement(clone, taskId);
     }
+    console.groupEnd();
 }
-function removeIssue(element){
+/**
+ * Удалить копию задачи с нижней панели
+ * @param element
+ */
+function removeIssueCopyFromPanel(element){
+    // clarify: Не нужно ли (и возможно ли?) добавить наблюдателей в цикле, убрав из тегов? И что с angular-подходом тогда?
+    console.groupCollapsed('%cremoveIssueCopyFromPanel', 'color:darkred', showArgs(arguments));
     var card=element.parentNode,
         taskId = getTaskId(card),
         draggedElementPanel = dragStore.getDragElement(taskId);
+    console.log({ card:card, taskId:taskId, draggedElementPanel:draggedElementPanel });
     /** удалить ранее сохранённый элемент, чтобы не блокировал
     повторное копирование данных */
     if(draggedElementPanel&&draggedElementPanel.innerHTML==card.innerHTML){
         dragStore.removeDraggedElementPanel(taskId);
     }
+    console.log('remove card', { cardParentNode: card.parentNode, card: card});
     card.parentNode.removeChild(card);
+    console.groupEnd();
 }
