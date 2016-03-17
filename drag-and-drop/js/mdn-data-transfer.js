e1da063831212e12f6efcdfbdca53123a434c707
srgg6701 <srgg67@gmail.com>
2016-03-17 19:51:52 +0300

    Унификация и рефакторинг.

--- drag-and-drop/js/mdn-data-transfer.js
+++ drag-and-drop/js/mdn-data-transfer.js
@@ -2,29 +2,26 @@
 function dragstart_handler(ev) {
     console.groupCollapsed('dragStart');
         console.log('ev.target', ev.target);
-    addClass(ev.currentTarget, 'start');
+    modifyClassList(ev.currentTarget, 2, true);
     ev.dataTransfer.setData("text", ev.target.id);
     // Tell the browser both copy and move are possible
     ev.effectAllowed = "copyMove";
     console.groupEnd();
 }
-
+//
 function dragenter_handler(ev){
-    console.groupCollapsed('Enter');
+    /*console.groupCollapsed('Enter');
     console.log('ev.target', ev.target);
-    console.groupEnd();
+    console.groupEnd();*/
 }
 // ev ─ пассивный элемент, может быть другим ранее активным элементом!
 function dragover_handler(ev) {
     ev.preventDefault();
-    addClass(ev.currentTarget, 'over');
+    modifyClassList(ev.currentTarget, 1, true);
 }
 // ev.currentTarget ─ пассивный элемент
 function dragleave_handler(ev) {
-    console.groupCollapsed('Leave');
-    console.log('ev.target', ev.target);
-    removeClass(ev.target, 'over');
-    console.groupEnd();
+    modifyClassList(ev.currentTarget, 1);
 }
 // droppingElement ─ активный элемент, извлекается из DOM по id
 // ev.currentTarget ─ пассивный элемент
@@ -50,8 +47,8 @@ function dragFinish(ev) {
                 node = node.cloneNode(true);
                 node.id = node.id + "_copy";
             }
-            removeClass(node, 'start');
-            removeClass(ev.target, 'over');
+            modifyClassList(ev.currentTarget, 1);
+            modifyClassList(node, 2);
             ev.target.appendChild(node);
         }
     } catch (e) {
@@ -67,37 +64,38 @@ function dragend_handler(ev) {
     ev.dataTransfer.clearData();
     /** • Тут нюанс. Отловить непосредственно элемент ev.target.parentNode
         не всегда возможно */
-    removeClass(ev.target, 'over');
-    removeClass(ev.target, 'start');
+    modifyClassList(ev.currentTarget, 1);
+    modifyClassList(ev.currentTarget, 2);
     /** проверить блоки с неудалённым классом 'drag-over'.
-        ситуация возможна, когда после события over не возникает
-        события leave для данного объекта, что происходит, если
+        ситуация возникает, когда после события *over* не наступает
+        событие *leave* для данного объекта, что происходит, если
         перемещаемый элемент не может быть на него сброшен. */
     var overRemains=document.getElementsByClassName('drag-over');
     if(overRemains.length){
         for(var i= 0, j=overRemains.length; i<j; i++){
-            removeClass(overRemains[i], 'over');
+            modifyClassList(overRemains[i], 1);
         }
     }
     console.groupEnd();
     console.log('- - - - - - - - - - - -');
 }
-
-function removeClass(element, name){
+//
+function modifyClassList(element, classNumber, add){
     if(element.classList) {
-        if(element.classList.contains('drag-'+name)) {
-            console.log('removeClass', {element: element, name: name});
-            element.classList.remove('drag-' + name);
-            console.trace();
+        var func, className='drag-';
+        switch (classNumber){
+            case 1:
+                className+='over';
+                break;
+            case 2:
+                className+='start';
+                break;
         }
-    }
-}
-function addClass(element, name){
-    if(element.classList) {
-        if(!element.classList.contains('drag-'+name)){
-            console.log('addClass', { element:element, name:name });
-            element.classList.add('drag-'+name);
-            console.trace();
+        if(add){
+            if(!element.classList.contains(className)) func='add';
+        }else{
+            if(element.classList.contains(className)) func='remove';
         }
+        if(func) element.classList[func](className);
     }
 }
