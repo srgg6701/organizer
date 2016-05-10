586418c46583861c9e4032110535a8df1c8bee18
srgg6701 <srgg67@gmail.com>
2016-05-11 00:36:58 +0300

    Выполнено тестирование загружаемого файла.

--- scripts/testing/jasmine/jasmine-jquery/spec/tests.js
+++ scripts/testing/jasmine/jasmine-jquery/spec/tests.js
@@ -1,27 +1,38 @@
-
-describe("Should count", function(){
+//jasmine.getFixtures().fixturesPath = 'fixture';
+// async: https://www.udacity.com/course/viewer#!/c-ud549/l-3773158892/m-3871168615
+xdescribe("Should count", function(){
     it("Check sum", function(){
         expect(1+1).toBe(2);
     });
 });
+describe('Set fixtures', function() {
+    beforeEach(function() {
+        setFixtures('<div id="elem">Hello World!</div>');
+    });
+
+    it('should be able to watch events', function() {
+        spyOnEvent('#elem', 'click');
+        $('#elem').click();
+        expect('click').toHaveBeenTriggeredOn('#elem');
+    });
+});
+
+describe('Load fixtures', function() {
+    beforeEach(function() {
+        loadFixtures('ul.html');
+    });
+    it('Should find unordered list', function(){
+        var list = $('#ul-1');
+        expect(list).not.toBeNull();
+        expect(list[0].id).toMatch('ul-1');
+    });
+});
 
 describe("jQuery tests", function () {
-    //console.log('$', $);
-
-    it("Should find jQuery", function () {
+    xit("Should find jQuery", function () {
         expect($).not.toBeNull();
     });
-    describe('jquery-jasmine plugin', function() {
-        beforeEach(function() {
-            setFixtures('<div id="elem">Hello World!</div>');
-        });
 
-        it('should be able to watch events', function() {
-            spyOnEvent('#elem', 'click');
-            $('#elem').click();
-            expect('click').toHaveBeenTriggeredOn('#elem');
-        });
-    });
     it("Should find checked box", function(){
         expect($('<input type="checkbox" checked="checked"/>')).toBeChecked();
     });
