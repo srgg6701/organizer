7b01e641695409ed4b65e32818f92131653a5b14
srgg6701 <srgg67@gmail.com>
2016-06-11 08:17:36 +0300

    Предварительная настройка роутера.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,30 @@
 var http = require('http'),
     url = require('url'),
     port = process.argv[2] || 9876,
+    pathnames,
     serverUrl = '127.0.0.1';
-
-console.log({port:port, serverUrl:serverUrl});
 
 http.createServer(runServer).listen(port,serverUrl);
 
 function runServer(req, res){
-    //var pathname = url.parse(req.url).pathname;
+    var path = url.parse(req.url).pathname;
+    pathnames = path.split('/');
     //console.log('Pathname: ', pathname);
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.write("Hello, Dude! Do anything, at last!");
+    res.write("<h1>Hello, Dude!</h1>");
+    pathnames.shift();
+    if(!pathnames[pathnames.length-1]) pathnames.pop();
+    var pathsLength = pathnames.length;
+    res.write("<h4>Path("+pathsLength+"): /"+pathnames.join('/')+"</h4>");
+    if(pathnames[0]){
+        res.write("<p>paths:</p>");
+        for (var i = 0, j = pathsLength; i < j; i++) {
+            if(pathnames[i])
+                res.write(pathnames[i]+'<br>');
+        }
+    }else{
+        res.write('<p>Point any path out, at last!</p>');
+    }
     res.end();
 }
 module.exports={
