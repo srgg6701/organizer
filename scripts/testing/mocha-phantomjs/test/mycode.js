4b4a0f3695cc5b5d725f9dd8634c028694ae215f
srgg6701 <srgg67@gmail.com>
2016-08-14 20:23:29 +0300

    Начало работы с mocha-phantomjs.

---
+++ scripts/testing/mocha-phantomjs/test/mycode.js
@@ -0,0 +1,7 @@
+
+describe("Test block 1", function () {
+    it("Initial checking", function () {
+        console.log('expectations here');
+        expect(1+1==2).to.be.true;
+    });
+});
--- scripts/testing/sinon/temp.js
+++ scripts/testing/sinon/temp.js
@@ -14,3 +14,8 @@ function setupNewUser(info, callback) {
 
 
 
+
+
+
+
+
