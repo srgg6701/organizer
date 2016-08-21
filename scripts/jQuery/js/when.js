971c499dbfd2e2f141f8a30a6223e4940c69df5c
srgg6701 <srgg67@gmail.com>
2016-08-21 18:00:23 +0300

    Добавлена функция с setInterval

--- scripts/jQuery/js/when.js
+++ scripts/jQuery/js/when.js
@@ -1,16 +1,35 @@
 $(function() {
 
     function wait(delay) {
-        console.log('wait, delay:', delay);
+        console.log('%cwait, delay:', 'color: rebeccapurple', delay);
         var nd = new Date;
         while (new Date - nd < delay);
+        console.log('I see, you waited for me so patiently.\nTime has passed:', (new Date - nd)/1000);
         return true;
+    }
+
+    function trustMe(delay) {
+        console.log('%ctrustMe, delay:', 'color:green', delay);
+        var tmt = setTimeout(function() {
+            console.log('I see, you trast me :)');
+            return true;
+        }, delay);
     }
 
     function runWaiter() {
         console.log('runWaiter here');
-        $.when(wait(1000), wait(2000)).done(function() {
-            console.log('Everything is done!');
+        var repeator = 500,
+            delayShort = 3000,
+            delayLong = 5000,
+            cnt = 0,
+            ntrv = setInterval(function() {
+                console.log('%ccnt: ' + ++cnt, 'color:blue');
+                if (cnt * repeator >= delayLong) {
+                    clearInterval(ntrv);
+                }
+            }, 500);
+        $.when(wait(3000), trustMe(5000)).done(function() {
+            console.log('%cEverything is done!', 'background-color:lightskyblue');
         }).fail(function() {
             console.log('You are fail, Dude!');
         });
