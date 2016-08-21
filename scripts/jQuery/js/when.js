fd9cc1b2072043eb1fc1bb94a3c8339327481585
srgg6701 <srgg67@gmail.com>
2016-08-21 19:04:42 +0300

    Пришло понимание смысла Deferred и when ☻

--- scripts/jQuery/js/when.js
+++ scripts/jQuery/js/when.js
@@ -4,31 +4,38 @@ $(function() {
         console.log('%cwait, delay:', 'color: rebeccapurple', delay);
         var nd = new Date;
         while (new Date - nd < delay);
-        console.log('I see, you waited for me so patiently.\nTime has passed:', (new Date - nd)/1000);
+        console.log('wait is finished.\nTime has passed:', (new Date - nd) / 1000);
         return true;
     }
 
     function trustMe(delay) {
         console.log('%ctrustMe, delay:', 'color:green', delay);
-        var tmt = setTimeout(function() {
-            console.log('I see, you trast me :)');
-            return true;
-        }, delay);
+        var tDef = $.Deferred(),
+            tmt = setTimeout(function() {
+                console.log('trustMe is finished');
+                tDef.resolve();
+            }, delay);
+        return tDef;
     }
 
     function runWaiter() {
         console.log('runWaiter here');
-        var repeator = 500,
-            delayShort = 3000,
-            delayLong = 5000,
-            cnt = 0,
-            ntrv = setInterval(function() {
-                console.log('%ccnt: ' + ++cnt, 'color:blue');
-                if (cnt * repeator >= delayLong) {
-                    clearInterval(ntrv);
-                }
-            }, 500);
-        $.when(wait(3000), trustMe(5000)).done(function() {
+        $.when(wait(2000), trustMe(4000)).done(function() {
+            console.log('%cEverything is done!', 'background-color:lightskyblue');
+        }).fail(function() {
+            console.log('You are fail, Dude!');
+        });
+    }
+
+    function loadData() {
+        console.log('Let\'s load data');
+        $.when($.get('http://localhost:8080/scripts/data/big-data.json', function(data) {
+                console.log('data: ', data);
+            }),
+            $.get('http://localhost:8080/scripts/data/big-data-copy.json', function(data1) {
+                console.log('data1: ', data1);
+            })
+        ).done(function() {
             console.log('%cEverything is done!', 'background-color:lightskyblue');
         }).fail(function() {
             console.log('You are fail, Dude!');
@@ -36,4 +43,5 @@ $(function() {
     }
 
     $('#btn-run-waiter').on('click', runWaiter);
+    $('#btn-load-data').on('click', loadData);
 })
