f7fe3c63cfd81463a26a4fee69c0d7dd6b10303d
srgg6701 <srgg67@gmail.com>
2017-10-14 12:57:52 +0300

    Adding next module

---
+++ scripts/modules/ES6/indigineous.js
@@ -0,0 +1,4 @@
+console.log('Level 2 module');
+export default function getPeople(){
+    console.log('Indigineous people here');
+}
---
+++ scripts/modules/native.js
@@ -0,0 +1,5 @@
+import * as all from "./ES6/indigineous.js";
+console.log('A native module here!');
+export default function consoleOutput() {
+    console.log('Arrow function here, all indigineous people here also=>', all);
+}
